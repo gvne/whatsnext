db397d302bb4084283330dbc9cfc6801dc1a04ed
gvne <guillaume.vincke@gmail.com>
2018-09-12 21:52:22 +0200

    Implement the client routing

--- whatsnext/src/app/app-routing.module.ts
+++ whatsnext/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { IndexComponent }   from './index/index.component';
 import { MasterComponent }   from './master/master.component';
+import { ClientComponent }   from './client/client.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/index', pathMatch: 'full' },
   { path: 'index', component: IndexComponent },
-  { path: 'master/:id', component: MasterComponent }
+  { path: 'master/:id', component: MasterComponent },
+  { path: 'client/:id', component: ClientComponent }
 ];
 
 @NgModule({
--- whatsnext/src/app/client/client.component.ts
+++ whatsnext/src/app/client/client.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-client',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./client.component.css']
 })
 export class ClientComponent implements OnInit {
+  private id: string;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.id = this.route.snapshot.paramMap.get('id');
   }
 
 }
