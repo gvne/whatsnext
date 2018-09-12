50e69558e03310ad295cb1e13166f07f8c1e63e4
gvne <guillaume.vincke@gmail.com>
2018-09-12 22:46:23 +0200

    Implement the song append from the client page

--- whatsnext/src/app/client/client.component.ts
+++ whatsnext/src/app/client/client.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { API_URL } from '../constants';
+import { Playlist, Song } from '../objects';
 
 @Component({
   selector: 'app-client',
@@ -8,13 +12,34 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ClientComponent implements OnInit {
   private id: string;
+  private video_id: string;
+  private error: string;
 
   constructor(
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private http: HttpClient
   ) { }
 
   ngOnInit() {
     this.id = this.route.snapshot.paramMap.get('id');
   }
 
+  appendVideo(video_id) {
+    const song: Song = {id: video_id, source: "youtube"};
+    let future = this.http.post<Playlist>(
+      API_URL + "/playlist/" + this.id + "/append",
+      JSON.stringify(song)
+    );
+
+    future.subscribe(
+      playlist => {
+        this.success_message = "Successfuly appended !";
+        setTimeout(() => { this.success_message = null; }, 5000);
+      }
+      error => {
+        this.error_message = "Something went wrong...";
+        setTimeout(() => { this.error_message = null; }, 5000);
+      }
+    );
+  }
 }
