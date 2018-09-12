9714876957332d34ef78cca81d9fa21fad8dc1ed
gvne <guillaume.vincke@gmail.com>
2018-09-12 20:00:39 +0200

    Implement the routing to master component on url host/master

--- whatsnext/src/app/app-routing.module.ts
+++ whatsnext/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { IndexComponent }   from './index/index.component';
+import { MasterComponent }   from './master/master.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/index', pathMatch: 'full' },
+  { path: 'index', component: IndexComponent },
+  { path: 'master', component: MasterComponent }
+];
+
 @NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
   exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
