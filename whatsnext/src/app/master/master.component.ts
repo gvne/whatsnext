90ddb1806242f555c0922cfb5d4baa6ae61093d3
gvne <guillaume.vincke@gmail.com>
2018-09-13 21:12:20 +0200

    Handle the moving to next song on the master page

--- whatsnext/src/app/master/master.component.ts
+++ whatsnext/src/app/master/master.component.ts
@@ -16,7 +16,7 @@ export class MasterComponent implements OnInit {
   player: YT.Player;
   private id: string;
   private video_id: string;
-  private playlistDoesNotExist: boolean;
+  private playlist_exists: boolean;
 
   constructor(
     private route: ActivatedRoute,
@@ -26,6 +26,7 @@ export class MasterComponent implements OnInit {
 
   ngOnInit() {
     this.id = this.route.snapshot.paramMap.get('id');
+    this.playlist_exists = true;
     // every 5 seconds we query the API to update the interface
     this.updateInterface();
   }
@@ -35,47 +36,66 @@ export class MasterComponent implements OnInit {
   }
 
   updateInterface() {
+    console.log("Updating !");
     let future = this.http.get<Playlist>(API_URL + "/playlist/" + this.id);
     future.subscribe(
       playlist => {
+        this.playlist_exists = true;
+        // We obtained a playlist.
         if (playlist.current_song) {
-          this.updateCurrentVideo(playlist.current_song.id);
+          if (this.video_id !== playlist.current_song.id) {
+            // Update the current song to the one obtained from the API if any
+            this.setCurrentVideo(playlist.current_song.id);
+          } else if (this.player.getPlayerState() === YT.PlayerState.ENDED) {
+            // If current video is still the same and player has ended,
+            // try to move to next song
+            this.requestNext();
+          }
         } else {
-          this.requestNext();
+          // move to next song if we don't have any current song and the songs
+          // list isn't empty
+          if (playlist.songs && playlist.songs.length !== 0) {
+            this.requestNext();
+          }
         }
-        // restart another update in 10seconds
-        setTimeout(() => { this.updateInterface(); }, 10000);
       }
       error => {
         if (error.status == 404) {
-          this.playlistDoesNotExist = true;
+          this.playlist_exists = false;
         }
         // TODO: handle other errors
-        // restart another update in 10seconds
+      }
+      () => {
+        // even if we get an error, we will retry in 10 seconds
         setTimeout(() => { this.updateInterface(); }, 10000);
-      });
-  }
-
-  requestNext() {
-    let future = this.http.post<Playlist>(
-      API_URL + "/playlist/" + this.id + "/next");
-    future.subscribe(
-      playlist => { this.updateInterface(); }
-      error => { this.updateInterface(); }
+      }
     );
   }
 
-  savePlayer(player) {
-    this.player = player;
+  requestNext() {
+    // post to the next enpoint and set the current video from obtained result
+    let future = this.http.post<Playlist>(
+      API_URL + "/playlist/" + this.id + "/next");
+    future.subscribe(
+      playlist => {
+        this.setCurrentVideo(playlist.current_song.id);
+      }
+    );
   }
 
-  updateCurrentVideo(video_id) {
-    // if the player isn't initialzed yet, just wait for the next update
-    if (!this.player) {
-      return;
-    }
+  // -------------------------------
+  // YoutubePlayer functions
+  // -------------------------------
+  setCurrentVideo(video_id) {
     // no need to update if the current id hasn't changed
     if (this.video_id === video_id) {
+      return;
+    }
+    // if the player isn't initialzed yet
+    if (!this.player) {
+      console.log("Player isn't properly initialized. Waiting 1 second")
+      // retry in one second !
+      setTimeout(() => { this.setCurrentVideo(video_id); }, 1000);
       return;
     }
     // update !
@@ -83,4 +103,37 @@ export class MasterComponent implements OnInit {
     this.player.loadVideoById(this.video_id);
     this.player.playVideo();
   }
+
+  savePlayer(player) {
+    this.player = player;
+  }
+
+  onStateChange(event) {
+    // event.data gives YT.PlayerState
+    switch(event.data) {
+      case YT.PlayerState.ENDED: {
+        // Player ends means we need to move to the next song
+        this.requestNext();
+        break;
+      }
+      case YT.PlayerState.PLAYING: {
+        break;
+      }
+      case YT.PlayerState.PAUSED: {
+        break;
+      }
+      case YT.PlayerState.BUFFERING: {
+        break;
+      }
+      case YT.PlayerState.CUED: {
+        break;
+      }
+      default: {
+        break;
+      }
+    }
+  }
+  // -------------------------------
+
+
 }
