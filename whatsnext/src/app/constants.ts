c209da81d8934c48116447ceee8c186b550dec04
gvne <guillaume.vincke@gmail.com>
2018-10-10 22:05:16 +0200

    Force the use of https on suggestion API

--- whatsnext/src/app/constants.ts
+++ whatsnext/src/app/constants.ts
@@ -1,7 +1,7 @@
 export const API_URL = "https://b3owi4hxn3.execute-api.us-east-1.amazonaws.com/Prod/v1";
 
 // Search related constant
-export const SEARCH_AUTOCOMPLETE_API_URL = "http://suggestqueries.google.com/complete/search";
+export const SEARCH_AUTOCOMPLETE_API_URL = "https://suggestqueries.google.com/complete/search";
 
 // Youtube Data API
 export const YOUTUBE_API_SEARCH_ENDPOINT = "https://www.googleapis.com/youtube/v3/search"
