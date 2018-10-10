ff276a6eb58bd234f7095d736be4f81b4372c0ed
gvne <guillaume.vincke@gmail.com>
2018-10-10 21:40:34 +0200

    Update the constants with youtube data api related stuff

--- whatsnext/src/app/constants.ts
+++ whatsnext/src/app/constants.ts
@@ -1,1 +1,8 @@
 export const API_URL = "https://b3owi4hxn3.execute-api.us-east-1.amazonaws.com/Prod/v1";
+
+// Search related constant
+export const SEARCH_AUTOCOMPLETE_API_URL = "http://suggestqueries.google.com/complete/search";
+
+// Youtube Data API
+export const YOUTUBE_API_SEARCH_ENDPOINT = "https://www.googleapis.com/youtube/v3/search"
+export const YOUTUBE_API_KEY = "AIzaSyCj2OsiC0wBxjD3gRdYaXr81ip_RlTM6A4";
