adbbf20da5193146c2e71412e72773c9de2b2e7a
gvne <guillaume.vincke@gmail.com>
2018-10-10 20:56:01 +0200

    Add angular Material

--- whatsnext/src/app/app.module.ts
+++ whatsnext/src/app/app.module.ts
@@ -7,23 +7,27 @@ import { YoutubePlayerModule } from 'ngx-youtube-player';
 import { AppComponent } from './app.component';
 import { IndexComponent } from './index/index.component';
 import { MasterComponent } from './master/master.component';
+import { SearchbarComponent } from './searchbar/searchbar.component';
 import { AppRoutingModule } from './/app-routing.module';
 
 import { WINDOW_PROVIDERS } from './window.provider';
 import { ClientComponent } from './client/client.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     IndexComponent,
     MasterComponent,
-    ClientComponent
+    ClientComponent,
+    SearchbarComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    YoutubePlayerModule
+    YoutubePlayerModule,
+    BrowserAnimationsModule,
   ],
   providers: [
     WINDOW_PROVIDERS
